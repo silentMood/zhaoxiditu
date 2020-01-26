54f61a5c86738c2e51e5bcaa82dd1639298f49ba
scott <cgyqqcgy@gmail.com>
2020-01-26 20:27:21 +0800

    fix: test

--- frontend/commitlint.config.js
+++ frontend/commitlint.config.js
@@ -2,5 +2,6 @@
  * extend from:
  * https://github.com/conventional-changelog/commitlint/blob/master/%40commitlint/config-conventional/index.js
  */
+
 module.exports = { extends: ['@commitlint/config-conventional'] };
 
