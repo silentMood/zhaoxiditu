c41f1a7fed1d0e99ada95dc34c07ce27ee212a63
silentmood <silentmood198909@gmail.com>
2020-01-26 16:04:45 +0800

    add the database

--- server/app/controller/home.js
+++ server/app/controller/home.js
@@ -5,6 +5,7 @@ const Controller = require('egg').Controller;
 class HomeController extends Controller {
   async index() {
     const { ctx } = this;
+    await ctx.model.User.create({name: "test", age: 26});
     ctx.body = 'hi, egg';
   }
 }
--- server/config/config.default.js
+++ server/config/config.default.js
@@ -18,6 +18,14 @@ module.exports = appInfo => {
   // add your middleware config here
   config.middleware = [];
 
+  // config the mysql
+  config.sequelize = {
+    dialect: 'mysql',
+    host: '127.0.0.1',
+    port: 3306,
+    database: 'test-zhaoxiditu',
+  };
+
   // add your user config here
   const userConfig = {
     // myAppName: 'egg',
