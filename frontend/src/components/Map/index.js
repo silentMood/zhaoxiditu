ebe2cf3074d0dca84339a4aa8dc34fa49959f420
silentmood <silentmood198909@gmail.com>
2020-01-27 16:01:25 +0800

    fix: set wuhan at map center

--- frontend/src/components/Map/index.js
+++ frontend/src/components/Map/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import styles from './map.component.css';
-import {
-  Provider,
-  KeepAlive
-} from 'react-keep-alive';
+import { Toast } from 'antd-mobile';
 
 class Map extends React.Component {
   constructor(props) {
@@ -11,7 +8,20 @@ class Map extends React.Component {
   }
 
   componentDidMount() {
-    this.zhaoximap = new AMap.Map('zhaoximapbox');
+    const map = this.__zhaoximap = new AMap.Map('zhaoximapbox', {
+      center: [114.31, 30.58],
+      zoom: 10
+    });
+    Toast.loading('初始化...')
+    map.on('complete', () => {
+      Toast.hide()
+    })
+  }
+
+  componentWillUnmount() {
+    if (this.__zhaoximap) {
+      this.__zhaoximap.destory()
+    }
   }
 
   render() {
