e62d2881011253b0eb5d818255bf4972e6de6811
silentmood <silentmood198909@gmail.com>
2020-01-27 16:35:53 +0800

    fix: wait for the issues

--- frontend/src/components/Map/index.js
+++ frontend/src/components/Map/index.js
@@ -8,15 +8,46 @@ class Map extends React.Component {
   }
 
   componentDidMount() {
+    this.initMap()
+  }
+
+  initMap () {
     const map = this.__zhaoximap = new AMap.Map('zhaoximapbox', {
       center: [114.31, 30.58],
-      zoom: 10
+      zoom: 9
     });
     Toast.loading('初始化...')
     map.on('complete', () => {
+      // lock area at wuhan
+      map.setLimitBounds(map.getBounds());
       Toast.hide()
     })
+
+    map.on('click', (info) => {
+      console.log(info, 'click position')
+    })
+
+    map.on('dblclick', (info) => {
+      console.log(info, 'dblclick position')
+    })
+
+    map.on('zoomend', () => {
+      const bounds = map.getBounds()
+      console.log(bounds, 'zoom end')
+    })
+
+    map.on('moveend', () => {
+      const bounds = map.getBounds()
+      console.log(bounds, 'move end')
+    })
+
+    map.on('dragend', () => {
+      const bounds = map.getBounds()
+      console.log(bounds, 'drag end')
+    })
   }
+
+  // addMarker 数据结构
 
   componentWillUnmount() {
     if (this.__zhaoximap) {
